4c7522ef6578252c7e3021572f8b7c63b37f2403
fmndantas <44785783+fmndantas@users.noreply.github.com>
2018-12-20 17:02:29 -0200

    up in Station class

--- Station.py
+++ Station.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 class Station:
     def __init__(self, name):
         self.name = name
@@ -5,30 +7,72 @@ class Station:
         self.distances = dict()
         self.channels = dict()
 
+        self.ChannelNamedTuple = namedtuple("Channel", "Type Amount")
+
     def __repr__(self):
         return self.name
 
-    def Link2Station(self, other_station, distance):
+    def SetNeighbor(self, other_station, distance):
+        """
+        :param other_station: the station intended to be set as neighbor
+        :param distance: distance between the current station and the other_station in km
+        """
         if other_station != self:
             self.neighborhood.add(other_station)
             for neighbor in self.neighborhood:
                 self.distances[neighbor] = distance
+
             other_station.neighborhood.add(self)
             for neighbor in other_station.neighborhood:
                 other_station.distances[neighbor] = distance
         else:
             print("You can\'t link a station to itself")
 
-    def SetChannels(self, neighbor, channels, type = "Normal"):
-        self.channels[neighbor] = [type, channels]
-        neighbor.channels = [type, channels]
+    def SetChannels(self, neighbor, channels, type="Normal"):
+        ChannelsToAppend = self.ChannelNamedTuple(type, channels)
+        if neighbor in self.neighborhood:
+            if neighbor not in self.channels.keys():
+                self.channels[neighbor] = []
+                self.channels[neighbor].append(ChannelsToAppend)
+            elif neighbor in self.channels.keys():
+                changeFlag = False
+                for item in self.channels[neighbor]:
+                    if item.Type == type:
 
-StationA = Station("Station A")
-StationB = Station("Station B")
+                        changeFlag = True
+                    else:
+                        pass
+                if not(changeFlag):
+                    self.channels[neighbor].append(ChannelsToAppend)
+                else:
+                    pass
+        else:
+            print("You can\'t set channels to a non neighbor station")
 
-StationA.Link2Station(StationB, 10)
-StationA.SetChannels(StationB, 15)
+    def ShowStation(self):
+        """
+        Shows the station in following form:
+        Name:
+        Neighborhood:
+        Distances to:
+        Channels to:
+        """
+        print("Name: {0}\n"
+              "Neighborhood: {1}"
+              "\nDistance to: {2}"
+              "\nChannels to: {3}".format(self, self.neighborhood, self.distances, self.channels))
 
-print(StationA.neighborhood)
-print(StationA.distances)
-print(StationA.channels)
+
+stationA = Station("Station A")
+stationB = Station("Station B")
+stationC = Station("Station C")
+
+stationA.SetNeighbor(stationB, 50)
+stationB.SetNeighbor(stationC, 42)
+
+stationA.SetChannels(stationB, 400, type="Normal")
+stationA.SetChannels(stationB, 600, type="Normal")
+
+stationA.SetChannels(stationB, 30, type="LT")
+stationA.ShowStation()
+
